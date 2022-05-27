1442fa4bc2f5306006b3b83f94ca981e41648bf6
David_Jan <37317200+davidjan76324@users.noreply.github.com>
2022-05-27 17:01:53 +0800

    Update wsgi.py

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -14,9 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_wsgi_application()
-
-def testprint():
-  print("test")
-
-def testprint2():
-  print("test")
