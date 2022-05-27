fa7fd09896ee044cd107bd38e8c62f880adcc525
davidjan76324 <david_jan76324@hotmail.com>
2022-05-27 16:26:55 +0800

    add two func

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -16,4 +16,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 application = get_wsgi_application()
 
 def testprint():
-  print("fuck")
+  print("test")
+
+def testprint2():
+  print("test")
