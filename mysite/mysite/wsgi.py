3121ee25f3916997d8e870bf9910df1f16f35eba
davidjan76324 <david_jan76324@hotmail.com>
2022-05-27 15:46:38 +0800

    @move same function position

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -6,39 +6,28 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
+def testalert():
+    print("testalert")
 
 import os
+def testalert():
+    print("testalert")
 
 from django.core.wsgi import get_wsgi_application
-
+def testalert():
+    print("testalert")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+def testalert():
+    print("testalert")
 
 application = get_wsgi_application()
 
 def testalert():
     print("testalert")
 
-def testalert():
-    print("testalert")
 
-def testalert():
-    print("testalert")
 
-def testalert():
-    print("testalert")
 
-def testalert():
-    print("testalert")
 
-def testalert():
-    print("testalert")
-def testalert():
-    print("testalert")
-def testalert():
-    print("testalert")
-def testalert():
-    print("testalert")
-def testalert():
-    print("testalert")
-def testalert():
-    print("testalert")
+
