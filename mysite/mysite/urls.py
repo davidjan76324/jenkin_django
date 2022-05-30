cc763164861263786142d2dc73eabf66dcc97d23
davidjan76324 <david_jan76324@hotmail.com>
2022-05-30 22:30:19 +0800

    @add one error line to test sonarQ email

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 import davidjan
+import davidjan
+asdkasmdo
+asdkasmdo
 asdkasmdo
 asdkasmdo
 
