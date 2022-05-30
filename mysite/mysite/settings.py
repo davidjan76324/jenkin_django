4754f265af116e73714f20414308f79bf1de2aa7
David_Jan <37317200+davidjan76324@users.noreply.github.com>
2022-05-30 10:02:07 +0800

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -105,3 +105,27 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+def printTest():
+    print("hello world!")
+def printTest():
+    print("hello world!")
+def printTest():
+    print("hello world!")
+def printTest():
+    print("hello world!")
+def printTest():
+    print("hello world!")
+def printTest():
+    print("hello world!")
+def printTest():
+    print("hello world!")
+def printTest():
+    print("hello world!")
+def printTest():
+    print("hello world!")
+def printTest():
+    print("hello world!")
+def printTest():
+    print("hello world!")
