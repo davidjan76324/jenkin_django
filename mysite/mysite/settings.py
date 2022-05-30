fe476f45129bbc1ae38b15c34f5c42c6e7f69c91
davidjan76324 <david_jan76324@hotmail.com>
2022-05-30 20:46:16 +0800

    @add

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = (
 )
 INSTALLED_APPS += ('django_jenkins',)
 JENKINS_TASKS = ( 
-    'django_jenkins.tasks.with_coverage',
     'django_jenkins.tasks.run_pylint',
 )
 PROJECT_APPS = ['mysite']
