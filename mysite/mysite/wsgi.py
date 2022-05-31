669bb77834c365ff81092982f544301972861b15
davidjan76324 <david_jan76324@hotmail.com>
2022-05-31 10:45:19 +0800

    @motify

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -9,35 +9,8 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 
 import os
-# import davidjan
-# asdkasmdo
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_wsgi_application()
-
-def printTest():
-    print("mytest")
-def printTest():
-    print("mytest")
-def printTest():
-    print("mytest")
-def printTest():
-    print("mytest")
-def printTest():
-    print("mytest")
-def printTest():
-    print("mytest")
-def printTest():
-    print("mytest")
-def printTest():
-    print("mytest")
-def printTest():
-    print("mytest")
-def printTest():
-    print("mytest")
-def printTest():
-    print("mytest")
-def printTest():
-    print("mytest")
